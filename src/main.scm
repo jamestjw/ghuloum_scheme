1b6395285f1f031f3dad7a37f1b72b2394e5fb76
James Tan <james_tan97@outlook.com>
2022-06-21 02:26:28 -0400

    Introduce special representations for immediate values

--- src/main.scm
+++ src/main.scm
@@ -4,13 +4,28 @@
   (apply fprintf out-file args)
   (newline out-file))
 
+(define fixnum-shift 2)
+(define char-shift 8)
+(define char-tag (string->number "00001111" 2))
+(define bool-shift 7)
+(define bool-tag (string->number "0011111" 2))
+(define empty-list-value (string->number "00101111" 2))
+
 (define (compile-program x)
- (emit "\t.text")
- (emit ".globl _scheme_entry")
- ; Not needed by Mach-O assembler
- ; (emit "\t.type scheme_entry, @function")
- (emit "_scheme_entry:")
- (emit "\tmovl $~a, %eax" x)
- (emit "\tret"))
+  (define (immediate-rep x)
+    (cond
+      ((integer? x) (ash x fixnum-shift))
+      ((char? x) (logior (ash (char->integer x) char-shift) char-tag))
+      ((boolean? x) (logior (ash (if x 1 0) bool-shift) bool-tag))
+      ((eq? x '()) empty-list-value)
+      (#t (error "compile-program" "Unexpected value type"))
+      ))
+  (emit "\t.text")
+  (emit ".globl _scheme_entry")
+  ; Not needed by Mach-O assembler
+  ; (emit "\t.type scheme_entry, @function")
+  (emit "_scheme_entry:")
+  (emit "\tmovl $~a, %eax" (immediate-rep x))
+  (emit "\tret"))
 
 (compile-program 5)
--- src/runtime.c
+++ src/runtime.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+#define fixnum_mask       3
+#define fixnum_tag        0
+#define fixnum_shift      2
+#define char_mask       255
+#define char_tag         15
+#define char_shift        8
+#define bool_mask       127
+#define bool_tag         31
+#define bool_shift        7
+#define empty_list       47
+
 extern int scheme_entry();
 
 int main(int argc, char** argv){
-  printf("%d\n", scheme_entry());
+  int val = scheme_entry();
+  if ((val & fixnum_mask) == fixnum_tag) {
+    printf("%d\n", val >> fixnum_shift);
+  } else if ((val & char_mask) == char_tag) {
+    printf("%c\n", val >> char_shift);
+  } else if ((val & bool_mask) == bool_tag) {
+    printf("%d\n", val >> bool_shift);
+  } else if (val == empty_list){
+    printf("()\n");
+  }
   return 0;
 }
