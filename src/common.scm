0a079080d8eca19af9bde40b50da536fe32c502a
James Tan <james_tan97@outlook.com>
2022-06-22 17:30:09 -0400

    Refactor primitive definition, add `char` and `sub1`

--- src/common.scm
+++ src/common.scm
@@ -1,7 +1,35 @@
 (library (common)
-  (export contains?)
-  (import (rnrs))  
+  (export contains? sexpr->string)
+  (import (rnrs))
+
   (define (contains? list x)
     (cond [(null? list) #f]
-  	[(equal? (car list) x) #t]
-  	[else (contains? (cdr list) x)])))
+     [(equal? (car list) x) #t]
+     [else (contains? (cdr list) x)]))
+
+  (define join
+    (lambda (l delim to-str)
+      (fold-left
+        (lambda (str elem)
+          (string-append str delim (to-str elem)))
+        (to-str (car l))
+        (cdr l))))
+
+  (define sexpr->string
+    (lambda (sexpr)
+      (cond
+        ((number? sexpr) (number->string sexpr))
+        ((symbol? sexpr) (symbol->string sexpr))
+        ((boolean? sexpr) (if sexpr "#t" "#f"))
+        ((string? sexpr) (string-append "\"" sexpr "\""))
+        ((char? sexpr) (string-append "#\\" (string sexpr)))
+        ((vector? sexpr)
+        (let ((s-vec (join (vector->list sexpr) " " sexpr->string)))
+          (string-append "#(" s-vec ")")))
+        ((null? sexpr) "()")
+        ((list? sexpr) (string-append "(" (join sexpr " " sexpr->string) ")"))
+
+        ((pair? sexpr)
+        (let ((s-car (sexpr->string (car sexpr)))
+              (s-cdr (sexpr->string (cdr sexpr))))
+          (string-append "(" s-car " . " s-cdr ")")))))))
--- src/compiler.scm
+++ src/compiler.scm
@@ -9,6 +9,7 @@
   (define fixnum2char-shift (- char-shift fixnum-shift))
   (define char2fixnum-shift fixnum2char-shift)
   (define char-tag (string->number "00001111" 2))
+  (define char-tag-mask (string->number "11111111" 2)) ; Eight bits (based on the shift)
   (define bool-shift 7)
   (define bool-tag (string->number "0011111" 2))
   (define bool-tag-mask (string->number "1111111" 2)) ; Seven bits (based on the shift)
@@ -38,89 +39,138 @@
       ((eq? x '()) empty-list-value)
       (else (error "compile-program" "Unexpected value type"))))
 
-  ; Assuming that `x` is a list, checks if the quoted expression
-  ; is a primitive call, e.g. (primcall? '(add1 2)) -> #t
-  (define (primcall? x)
-    (contains? '(add1 integer->char char->integer null? zero? integer? boolean? not) (car x)))
-
   ; (define (primcall-op x)
   ;   (case (car x)
   ;     ; TODO: Make this more DRY (consider returning just (car x)?)
   ;     ((add1) 'add1)
   ;     (else (error "primcall-op" "Invalid primitive operator"))))
-  
-  ; Assume that (primcall? x) -> #true
-  (define (primcall-op x)
-    (car x))
 
-  ; First argument of a primitive call
-  (define (primcall-operand1 x) (eval (cadr x)))
+  ; Make it more convenient to define a primitive
+  ; Binds the argument count and the code to emit
+  ; the function call to a symbol corresponding
+  ; to the name of the primitive
+  (define-syntax define-primitive
+    (syntax-rules ()
+     [(_ (prim-name out-port arg* ...) b b* ...)
+      (begin
+        (putprop 'prim-name '*is-prim* #t)
+        (putprop 'prim-name '*arg-count* (length '(arg* ...)))
+        (putprop 'prim-name '*emitter* (lambda (out-port arg* ...) b b* ...)))]))
 
-  (define (emit-expr out-port x)
+  (define (primitive? x)
+    (and (symbol? x) (getprop x '*is-prim*)))
+
+  (define (primitive-emitter x)
+    (or (getprop x '*emitter*) (error "primitive-emitter" "Invalid primitive")))
+
+  ; Verifies that `x` is a list, and then checks that
+  ; the first symbol corresponds to a primitive
+  (define (primcall? x)
+    (and (pair? x) (primitive? (car x))))
+
+  (define (check-primcall-args prim args)
+    (if (= (length args) (getprop prim '*arg-count*))
+      '() ; Do nothing if the lengths match
+      (error "check-primcall-args" (string-append "Invalid number of arguments for " (symbol->string prim)))))
+
+  (define (emit-primcall output-port expr)
+    (let ([prim (car expr)] [args (map eval (cdr expr))])
+      (check-primcall-args prim args) ; Verify that args are valid for this primcall
+      (apply (primitive-emitter prim) output-port args)))
+
+  (define (emit-expr out-port expr)
     (cond
-      [(immediate? x)
-          (emit out-port "\tmovl $~a, %eax" (immediate-rep x))]
-      [(primcall? x)
-        (case (primcall-op x)
-          [(add1)
-            (emit-expr out-port (primcall-operand1 x))
-            (emit out-port "\taddl $~a, %eax" (immediate-rep 1))]
-          [(integer->char)
-            (emit-expr out-port (primcall-operand1 x))
-            (emit out-port "\tshl $~a, %eax" fixnum2char-shift)
-            (emit out-port "\tor $~a, %eax" char-tag)]
-          [(char->integer)
-            (emit-expr out-port (primcall-operand1 x))
-            (emit out-port "\tshr $~a, %eax" char2fixnum-shift)]
-          [(zero?)
-            (if (integer? (primcall-operand1 x))
-                (begin (emit-expr out-port (primcall-operand1 x))
-                 (emit out-port "\tcmpl $0, %eax")              ; Compares %eax to 0
-                 (emit out-port "\tmovl $0, %eax")              ; Zeroes %eax
-                 (emit out-port "\tsete %al")                   ; Set lower byte of %eax to 1 if comparison was successful
-                 (emit out-port "\tsall $~a, %eax" bool-shift)  ; Apply appropriate shift
-                 (emit out-port "\torl $~a, %eax" bool-tag))    ; and tag
-                (error "emit-expr" "zero? can only be called with integers"))]
-          [(null?)
-            (emit-expr out-port (primcall-operand1 x))
-            (emit out-port "\tcmpl $~a, %eax" empty-list-value) ; Compares %eax to empty-list-value
-            (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
-            (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
-            (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
-            (emit out-port "\torl $~a, %eax" bool-tag)]         ; and tag
-          [(integer?)
-            (emit-expr out-port (primcall-operand1 x))
-            (emit out-port "\tand $~a, %eax" fixnum-tag-mask)   ; Gets the tag of a fixnum
-            (emit out-port "\tcmpl $~a, %eax" fixnum-tag)       ; Compares %eax to fixnum-tag
-            (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
-            (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
-            (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
-            (emit out-port "\torl $~a, %eax" bool-tag)]         ; and tag
-          [(boolean?)
-            (emit-expr out-port (primcall-operand1 x))
-            (emit out-port "\tand $~a, %eax" bool-tag-mask)     ; Gets the tag of a boolean
-            (emit out-port "\tcmpl $~a, %eax" bool-tag)         ; Compares %eax to bool-tag
-            (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
-            (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
-            (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
-            (emit out-port "\torl $~a, %eax" bool-tag)]         ; and tag
-          [(not)
-            (emit-expr out-port (primcall-operand1 x))
-            (emit out-port "\tcmpl $~a, %eax" bool-tag)         ; Compares %eax to the representation of false
-            (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
-            (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
-            (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
-            (emit out-port "\torl $~a, %eax" bool-tag)]         ; and tag
-          [else (error "emit-expr" "Unknown primitive operator")])]
-      ;[else (error "emit-expr" "Unknown expression encountered")]))
-      [else (begin (display x) (error "emit-expr" "Unknown expression encountered"))]))
+      [(immediate? expr)
+          (emit out-port "\tmovl $~a, %eax" (immediate-rep expr))]
+      [(primcall? expr) (emit-primcall out-port expr)]
+      [else (error "emit-expr" (string-append "Unknown expression " (sexpr->string expr) " encountered"))]))
 
   (define (compile-program x)
    (define of (out-file))
- 
+
    (emit-preamble of)
    (emit-expr of x)
    (emit of "\tret")
- 
+
    (flush-output-port of)
-   (close-port of)))
+   (close-port of))
+
+  ; ******* Definition of primitives ******
+  (define-primitive (add1 out-port arg)
+    (if (integer? arg)
+      (begin
+        (emit-expr out-port arg)
+        (emit out-port "\taddl $~s, %eax" (immediate-rep 1)))
+      (error "emit-expr" "add1? can only be called with integers")))
+
+  (define-primitive (sub1 out-port arg)
+    (if (integer? arg)
+      (begin
+        (emit-expr out-port arg)
+        (emit out-port "\tsubl $~s, %eax" (immediate-rep 1)))
+      (error "emit-expr" "sub1? can only be called with integers")))
+
+  (define-primitive (integer->char out-port arg)
+    (emit-expr out-port arg)
+    (emit out-port "\tshl $~a, %eax" fixnum2char-shift)
+    (emit out-port "\tor $~a, %eax" char-tag))
+
+  (define-primitive (char->integer out-port arg)
+    (emit-expr out-port arg)
+    (emit out-port "\tshr $~a, %eax" char2fixnum-shift))
+
+  (define-primitive (zero? out-port arg)
+    (if (integer? arg)
+      (begin
+        (emit-expr out-port arg)
+        (emit out-port "\tcmpl $0, %eax")              ; Compares %eax to 0
+        (emit out-port "\tmovl $0, %eax")              ; Zeroes %eax
+        (emit out-port "\tsete %al")                   ; Set lower byte of %eax to 1 if comparison was successful
+        (emit out-port "\tsall $~a, %eax" bool-shift)  ; Apply appropriate shift
+        (emit out-port "\torl $~a, %eax" bool-tag))    ; and tag
+      (error "emit-expr" "zero? can only be called with integers")))
+
+  (define-primitive (null? out-port arg)
+    (emit-expr out-port arg)
+    (emit out-port "\tcmpl $~a, %eax" empty-list-value) ; Compares %eax to empty-list-value
+    (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
+    (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
+    (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
+    (emit out-port "\torl $~a, %eax" bool-tag))         ; and tag
+
+  (define-primitive (not out-port arg)
+     (emit-expr out-port arg)
+     (emit out-port "\tcmpl $~a, %eax" bool-tag)         ; Compares %eax to the representation of false
+     (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
+     (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
+     (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
+     (emit out-port "\torl $~a, %eax" bool-tag))         ; and tag
+
+  (define-primitive (integer? out-port arg)
+    (emit-expr out-port arg)
+    (emit out-port "\tand $~a, %eax" fixnum-tag-mask)     ; Gets the tag of a fixnum
+    (emit out-port "\tcmpl $~a, %eax" fixnum-tag)         ; Compares %eax to a tag of a fixnum
+    (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
+    (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
+    (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
+    (emit out-port "\torl $~a, %eax" bool-tag))         ; and tag
+
+  (define-primitive (boolean? out-port arg)
+    (emit-expr out-port arg)
+    (emit out-port "\tand $~a, %eax" bool-tag-mask)     ; Gets the tag of a boolean
+    (emit out-port "\tcmpl $~a, %eax" bool-tag)         ; Compares %eax to bool-tag
+    (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
+    (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
+    (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
+    (emit out-port "\torl $~a, %eax" bool-tag))         ; and tag
+
+  (define-primitive (char? out-port arg)
+    (emit-expr out-port arg)
+    (emit out-port "\tand $~a, %eax" char-tag-mask)     ; Gets the tag of a char
+    (emit out-port "\tcmpl $~a, %eax" char-tag)         ; Compares %eax to char-tag
+    (emit out-port "\tmovl $0, %eax")                   ; Zeroes %eax
+    (emit out-port "\tsete %al")                        ; Set lower byte of %eax to 1 if comparison was successful
+    (emit out-port "\tsall $~a, %eax" bool-shift)       ; Apply appropriate shift
+    (emit out-port "\torl $~a, %eax" bool-tag))         ; and tag
+  ; ******* Definition of primitives ******
+)
